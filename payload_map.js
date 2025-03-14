6b098ae394b7ac5a31c8aab05c03148799ea1d8e
PS4IDEA <76072886+PS4IDEA@users.noreply.github.com>
2025-03-14 12:19:17 -0700

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -35,6 +35,17 @@ const payload_map = [
 	{
         displayTitle: "etaHEN",
         description: "AIO HEN",
+        fileName: "etaHENByLM.bin",
+        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "2.0b",
+        supportedFirmwares: ["5."],
+        toPort: 9021
+    },
+	{
+        displayTitle: "etaHEN",
+        description: "AIO HEN",
         fileName: "etaHEN.bin",
         author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
         projectSource: "https://github.com/LightningMods/etaHEN",
